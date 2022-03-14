6238b36bb046b8aded9bbc5a757f755eb7ec3ce9
glopesdev <glopesdev@users.noreply.github.com>
2022-03-14 20:57:44 +0000

    Update python version

--- python/setup.ps1
+++ python/setup.ps1
@@ -1,8 +1,8 @@
 if (!(Test-Path "./winpython")) {
-    Invoke-WebRequest "https://github.com/winpython/winpython/releases/download/4.1.20210417/Winpython64-3.8.9.0dot.exe" -OutFile "temp.exe"
+    Invoke-WebRequest "https://github.com/winpython/winpython/releases/download/4.3.20210620/Winpython64-3.8.10.0dot.exe" -OutFile "temp.exe"
 	$process = Start-Process "temp.exe" "-y" -PassThru
 	$process.WaitForExit()
-	Rename-Item -Path "WPy64-3890" -NewName "winpython"
+	Rename-Item -Path "WPy64-38100" -NewName "winpython"
     Remove-Item -Path "temp.exe"
 }
 
